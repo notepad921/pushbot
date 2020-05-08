b839b06a6636dbb5066607e59ddfa5db2d07efc7
Бондарь Ольга Александровна <o.bondar@mcplat.ru>
2020-05-08 12:39:52 +0300

    refactoring for pythonanyware

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-TOKEN = os.environ.get("TOKEN")
-
-name = "@olgabondar"
-chat_id = 188594475
-text = f"{name}, ты сегодня дежуришь"
--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from datetime import date
 import telebot
-import config
-
-bot = telebot.TeleBot(config.TOKEN)
 
 
-@bot.message_handler(commands = ['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Петрович в деле")
+TOKEN = "1209122889:AAEQocM53fjTteFxXsb8G5NZmbnPrRXKcv8"
+
+persons = {"Ольга": "@olgabondar",
+             "Алина": "@alice_in_the_field",
+             "Ильяс": "@irtimir",
+             "Дмитрий": "@zvrvdmtr",
+             "Кирилл": "@butusk0",
+             "Антон": "@qqwerty228"}
+
+chat_id = -494735017
 
 
-# sendMessage
-bot.send_message(config.chat_id, config.text)
+def check_weekday():
+    local_weekday = date.today().isoweekday()
+    return local_weekday
+
+
+def choose_person(local_weekday):
+    if local_weekday is 1:
+        local_person = persons.get("Алина")
+    elif local_weekday is 2:
+        local_person = persons.get("Кирилл")
+    elif local_weekday is 3:
+        local_person = persons.get("Антон")
+    elif local_weekday is 4:
+        local_person = persons.get("Дмитрий")
+    elif local_weekday is 5:
+        local_person = persons.get("Ильяс")
+    else:
+        return None
+    return local_person
+
+
+def send_push(local_chat_id, local_person):
+    text = f"{local_person}, ты сегодня дежуришь"
+    bot.send_message(local_chat_id, text)
+
+
+def send_text(local_chat_id, local_person):
+    text = f"{local_person}, ты сегодня дежуришь!"
+    print(text)
+
+
+bot = telebot.TeleBot(TOKEN)
+
+weekday = check_weekday()
+person = choose_person(weekday)
+send_text(chat_id, person)
